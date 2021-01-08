343fdd37805c5d19798e08e4d9c7f35a987ee59d
CoSmOS <mrskvayzer@gmail.com>
2021-01-08 15:18:50 +0300

    radioButtons

--- app/src/main/java/com/example/samsunghackathon2021/MainActivity.java
+++ app/src/main/java/com/example/samsunghackathon2021/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
@@ -13,11 +16,14 @@ import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+
 public class MainActivity extends AppCompatActivity {
     MqttHelper mqttHelper;
 
     TextView dataReceived;
     TextView temperature;
+    RadioButton btn_manual, btn_ontime, btn_auto;
+    ProgressBar ground, air;
 
     @Override
 
@@ -26,6 +32,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         temperature = (TextView) findViewById(R.id.temperature);
         dataReceived = (TextView) findViewById(R.id.dataReceived);
+        btn_manual = (RadioButton) findViewById(R.id.radio_manual);
+        btn_ontime = (RadioButton) findViewById(R.id.radio_ontime);
+        btn_auto = (RadioButton) findViewById(R.id.radio_auto);
+        btn_manual.setOnClickListener(radioButtonClickListener);
+        btn_ontime.setOnClickListener(radioButtonClickListener);
+        btn_auto.setOnClickListener(radioButtonClickListener);
+
+        ground = (ProgressBar) findViewById(R.id.progressGroundHum);
+        air = (ProgressBar) findViewById(R.id.progressAirHum);
 
         Handler handler = new Handler(){
             @Override
@@ -35,13 +50,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         handler.sendEmptyMessage(0);
-        Handler handler1 = new Handler();
-        handler1.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                //mqttHelper.publishMessage("CAN YOU HEAR MEEE?");
-            }
-        },1000);
+
 
     }
 
@@ -63,7 +72,15 @@ public class MainActivity extends AppCompatActivity {
                 Log.w("Debug", mqttMessage.toString());
                 dataReceived.setText(mqttMessage.toString());
                 String[] answer = mqttMessage.toString().split(" ");
-                temperature.setText(answer[0]);
+                double d_temp = Double.valueOf(answer[0]);
+                double d_hum = Double.valueOf(answer[1]);
+                int temp = (int) Math.ceil(d_temp);
+                int hum = (int) Math.ceil(d_hum);
+                temperature.setText("+" + temp);
+
+                //air.setMaxWidth(100);
+                air.setProgress(hum);
+
             }
 
             @Override
@@ -73,5 +90,43 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    View.OnClickListener radioButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            RadioButton rb = (RadioButton)v;
+            clearRadioChecked();
+            switch (rb.getId()) {
+                case R.id.radio_manual:
+                    btn_manual.setChecked(!btn_manual.isChecked());
+                    sendMessage(1);
+                    break;
+                case R.id.radio_ontime:
+                    btn_ontime.setChecked(!btn_ontime.isChecked());
+                    sendMessage(2);
+                    break;
+                case R.id.radio_auto:
+                    btn_auto.setChecked(!btn_auto.isChecked());
+                    sendMessage(3);
+                    break;
 
+                default:
+                    break;
+            }
+        }
+    };
+    public void clearRadioChecked() {
+        btn_manual.setChecked(false);
+        btn_auto.setChecked(false);
+        btn_ontime.setChecked(false);
+    }
+    public void sendMessage(int opcode){
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mqttHelper.publishMessage(""+opcode);
+            }
+        },1);
+    }
 }
