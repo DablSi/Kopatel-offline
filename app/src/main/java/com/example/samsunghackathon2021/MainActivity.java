3c8147634cdd0174fe49d9af47ce04c74f4c9af3
CoSmOS <mrskvayzer@gmail.com>
2021-01-08 12:17:14 +0300

    SOME KIND OF CLIENT WITHOUT GUI

---
+++ app/src/main/java/com/example/samsunghackathon2021/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.samsunghackathon2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class MainActivity extends AppCompatActivity {
+    MqttHelper mqttHelper;
+
+    TextView dataReceived;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dataReceived = (TextView) findViewById(R.id.dataReceived);
+
+        Handler handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg){
+                startMqtt();
+            }
+        };
+        handler.sendEmptyMessage(0);
+
+    }
+
+    private void startMqtt() {
+        mqttHelper = new MqttHelper(getApplicationContext(),"user_f70f4807/test/temp");
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w("Debug", mqttMessage.toString());
+                dataReceived.setText(mqttMessage.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+    }
+}
